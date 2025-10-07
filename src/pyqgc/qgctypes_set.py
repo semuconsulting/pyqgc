5320eb2208b8a442ae0068894cc577f71d388c6b
SEMU Admin <28569967+semuadmin@users.noreply.github.com>
2025-10-07 11:03:40 +0100

    update docstrings

--- src/pyqgc/qgctypes_set.py
+++ src/pyqgc/qgctypes_set.py
@@ -3,7 +3,7 @@ QGC Protocol command payload definitions.
 
 Created on 6 Oct 2025
 
-Information sourced from public domain Quectel Interface Specifications © 2013-2021, Quectel AG
+Information sourced from public domain Quectel Interface Specifications © 2025, Quectel
 https://www.quectel.com/download/quectel_lg290p03lgx80p03_gnss_protocol_specification_v1-1/
 
 :author: semuadmin (Steve Smith)
